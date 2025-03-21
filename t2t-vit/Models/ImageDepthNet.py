aface026e00d0fafdf2793784baa63492e304927
XZ-Jia <148881525+XZ-Jia@users.noreply.github.com>
2025-03-21 23:46:01 +0800

    Update ImageDepthNet.py

--- t2t-vit/Models/ImageDepthNet.py
+++ t2t-vit/Models/ImageDepthNet.py
@@ -4,6 +4,31 @@ from .t2t_vit import T2t_vit_t_14,T2t_vit_12,T2t_vit_10,T2t_vit_7
 from .Blocks import TFM
 from .Convdecoder import decoder
 
+def Mae(Mask, GT):
+    _,_,GW,GH = GT.shape
+    if Mask.size()[2:] != GT.size()[2:]:
+        Mask = F.interpolate(Mask, size=GT.size()[2:], mode='bilinear', align_corners=False)
+    Mask = Mask.sigmoid().data.cpu().numpy().squeeze()
+    GT = GT.sigmoid().data.cpu().numpy().squeeze()
+    Mask = (Mask - Mask.min()) / (Mask.max() - Mask.min() + 1e-8)
+    mae = np.sum(np.abs(Mask - GT)) * 1.0 / (GW * GH)
+    return mae
+
+
+# def Compare_mae(pre_rgb, pre_depth, pre_rgbd):
+#     mae = Mae(pre_depth, pre_rgbd)
+#     if mae>0.3:
+#         return True
+
+def Compare_iou(pre_rgb, pre_depth, pre_rgbd):
+    map_d = torch.sign(pre_depth)
+    map_rd = torch.sign(pre_rgbd)
+    map1 = map_d + map_rd
+    map2 = map1 + 1
+    IOU = torch.count_nonzero(map1 > 0.1)/torch.count_nonzero(map2 > 0.1)
+    if IOU< 0.1:
+        return True
+
 
 class Encoder(nn.Module):
     def __init__(self, args):
@@ -24,6 +49,12 @@ class Encoder(nn.Module):
         pre_rgb = self.score[0](siameae_fea_16.chunk(2)[0])
         pre_depth = self.score[1](siameae_fea_16.chunk(2)[1])
         pre_rgbd = self.score[2](fea_16)
+
+        # if Compare_new(pre_rgb, pre_depth, pre_rgbd):
+        #     rgb_rgb_toekn_16 = torch.cat((siameae_token_16.chunk(2)[0],siameae_token_16.chunk(2)[0]),dim=0)
+        #     fea_16 = self.CMF(rgb_rgb_toekn_16)
+        #     pre_rgbd = self.score[2](fea_16)
+        
         top_preds.append(pre_rgb)
         top_preds.append(pre_depth)
         top_preds.append(pre_rgbd)
